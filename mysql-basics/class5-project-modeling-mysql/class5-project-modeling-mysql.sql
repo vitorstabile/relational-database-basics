905378494781f2a9729e7b51ab1eedd70769401f
vitorstabile <vitor.stabile@hotmail.com>
2020-12-21 22:38:26 -0200

    class 5 push

---
+++ mysql-basics/class5-project-modeling-mysql/class5-project-modeling-mysql.sql
@@ -0,0 +1,24 @@
+
+/* CONCEPTUAL MODELING:
+		
+		 This Data Model defines WHAT the system contains. 
+		 This model is typically created by Business stakeholders and Data Architects. 
+		 The purpose is to organize, scope and define business concepts and rules.
+			
+*/
+
+/* LOGICAL MODELING:
+		
+		 Defines HOW the system should be implemented regardless of the DBMS. 
+		 This model is typically created by Data Architects and Business Analysts.
+		 The purpose is to developed technical map of rules and data structures.
+
+*/
+
+/* PHYSICAL MODELING:
+		
+		 This Data Model describes HOW the system will be implemented using a specific DBMS system.
+		 This model is typically created by DBA and developers.
+		 The purpose is actual implementation of the database.
+		 
+*/
